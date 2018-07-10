2724489312b76bdab388de4ec3e20866be0de143
Terran Webb <terranrw@gmail.com>
2018-07-10 12:50:54 -0400

    Add Submit DOM to JS file

--- designs.js
+++ designs.js
@@ -5,7 +5,7 @@ let width = document.querySelector('#inputWeight');
 
 let height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
-
+let submitBtn = document.querySelector('input[type=submit]');
 function makeGrid() {
 
 // Your code goes here!
