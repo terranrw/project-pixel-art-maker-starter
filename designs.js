9134cc4eef9d31e8304108f3efc3293eb063af31
Terran Webb <terranrw@gmail.com>
2018-07-10 13:46:03 -0400

    Change let to const for inputs

--- designs.js
+++ designs.js
@@ -1,9 +1,9 @@
 // Select color input
-let colorPicker = document.querySelector('#colorPicker');
+const colorPicker = document.querySelector('#colorPicker');
 // Select size input
-let width = document.querySelector('#inputWeight');
+const width = document.querySelector('#inputWeight');
 
-let height = document.querySelector('#inputHeight');
+const height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
 let submitBtn = document.querySelector('input[type=submit]');
 submitBtn.addEventListener('click', function (evt) {
