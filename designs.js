9ab06d51af2385d82f54f59f499bea16c96c5081
Terran Webb <terranrw@gmail.com>
2018-07-10 14:59:43 -0400

    Reformatted event listener for submit button and makegrid function

--- designs.js
+++ designs.js
@@ -6,15 +6,12 @@ const width = document.querySelector('#inputWeight');
 const height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
 let submitBtn = document.querySelector('input[type=submit]');
-submitBtn.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  makeGrid();
-})
+submitBtn.addEventListener('click', makeGrid);
 
-function makeGrid() {
+function makeGrid(evt) {
+  evt.preventDefault();
   let m = height.valueAsNumber;
   let n = width.valueAsNumber;
   const table = document.querySelector('#pixelCanvas');
-// Your code goes here!
-
+  
 }
