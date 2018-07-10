efb2f026b623f5f47c8e16ed97ce5afe75fed3fa
Terran Webb <terranrw@gmail.com>
2018-07-10 17:47:33 -0400

    Working makeGrid fuction using nested for loops and DOM

--- designs.js
+++ designs.js
@@ -5,7 +5,7 @@ const width = document.querySelector('#inputWeight');
 
 const height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
-let submitBtn = document.querySelector('input[type=submit]');
+const submitBtn = document.querySelector('input[type=submit]');
 submitBtn.addEventListener('click', makeGrid);
 
 function makeGrid(evt) {
@@ -13,5 +13,18 @@ function makeGrid(evt) {
   let m = height.valueAsNumber;
   let n = width.valueAsNumber;
   const table = document.querySelector('#pixelCanvas');
-  
+  const tableBody = document.createElement('tbody');
+//  const addTd = document.createElement('td');
+//  console.log(`${m} and ${n}`);
+// Your code goes here!
+  for (var row = 0; row < m; row++) {
+    const addTr = document.createElement('tr');
+
+    for (var col = 0; col < n; col++) {
+      const addTd = document.createElement('td');
+      addTr.appendChild(addTd);
+    }
+    tableBody.appendChild(addTr);
+  }
+  table.appendChild(tableBody);
 }
