854fdbb7061e294fb8ffe732a9cc97a3ded1513d
Terran Webb <terranrw@gmail.com>
2018-07-10 11:35:54 -0400

    Add inputWeight and inputHeight DOM to JS file

--- designs.js
+++ designs.js
@@ -1,7 +1,9 @@
 // Select color input
 let colorPicker = document.querySelector('#colorPicker');
 // Select size input
+let width = document.querySelector('#inputWeight');
 
+let height = document.querySelector('#inputHeight');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
